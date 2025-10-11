9e55415ac9e6a92ea91b7f9c3c78f9afb014a8b7
Joseph Micallef <micallefje@gmail.com>
2025-10-11 22:19:33 +0100

    update vpc.tf with vpc resource

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,5 +1,7 @@
 resource "aws_security_group" "allow-ssh" {
 	name = "allow_ssh"
+    description = "Allow SSH inbound traffic"
+    vpc_id = aws_vpc.main.id
 	
 	ingress {
         from_port = 22
@@ -7,4 +9,21 @@ resource "aws_security_group" "allow-ssh" {
 		protocol = "tcp"
 		cidr_blocks = ["0.0.0.0/0"] 
     }
+
+    egress {
+        from_port = 0
+		to_port = 0
+		protocol = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "my-vpc"
+  }
 }
