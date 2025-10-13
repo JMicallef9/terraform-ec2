09dbfc2574f118370fa467abfb8980c354ab731f
Joseph Micallef <micallefje@gmail.com>
2025-10-13 13:18:55 +0100

    add ec2.tf file

---
+++ terraform/ec2.tf
@@ -0,0 +1,18 @@
+data "aws_ami" "AMI_al2023" {
+	most_recent = true
+	filter {
+		name = "name"
+		values = ["al2023-ami-2023*-x86_64"]
+		}
+}
+
+resource "aws_instance" "access_all_areas" {
+	ami = data.aws_ami.AMI_al2023.id
+	instance_type = "t2.micro"
+
+	key_name = "access_key"
+	vpc_security_group_ids = [aws_security_group.allow-ssh.id]
+	tags = {
+		Name = "access_all_areas"
+		}
+}
