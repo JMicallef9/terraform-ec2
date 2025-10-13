0fbff9e72c9483b05f7bf0161c888b4cd15e8892
Joseph Micallef <micallefje@gmail.com>
2025-10-13 13:16:43 +0100

    update vpc.tf with subnet and route table

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -19,11 +19,48 @@ resource "aws_security_group" "allow-ssh" {
 }
 
 resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
   enable_dns_hostnames = true
 
   tags = {
     Name = "my-vpc"
   }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "eu-west-2"
+  map_public_ip_on_launch = true             
+
+  tags = {
+    Name = "main-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "gateway" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gateway.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.public.id
 }
