81ac91543d487ad55158911ab19753d3418649b1
Joseph Micallef <micallefje@gmail.com>
2025-10-16 13:05:05 +0100

    add instance profile for EC2 role

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -37,3 +37,8 @@ resource "aws_iam_role_policy" "s3_access_policy" {
     ]
   })
 }
+
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "ec2-instance-profile"
+  role = aws_iam_role.ec2_role.name
+}
