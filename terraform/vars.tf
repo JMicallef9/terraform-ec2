fac54c63a1e8c9615712e90d17ad85b8459baf82
Joseph Micallef <micallefje@gmail.com>
2025-10-19 11:31:46 +0100

    add file containing terraform variables

---
+++ terraform/vars.tf
@@ -0,0 +1,21 @@
+variable "bucket_name" {
+    description = "S3 bucket name"
+    type = string
+}
+
+variable "local_filepath" {
+    description = "Local file to upload before EC2 runs"
+    type = string
+}
+
+variable "input_key" {
+    description = "S3 key for uploaded input file"
+    type = string
+    default = "input/file.txt"
+}
+
+variable "output_key" {
+    description = "S3 key for processed CSV output"
+    type = string
+    default = "output/result.csv"
+}
