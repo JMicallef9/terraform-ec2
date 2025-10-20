bf133010e61411f3458b9226f56d693c47907a05
Joseph Micallef <micallefje@gmail.com>
2025-10-20 11:11:04 +0100

    add terraform_data resource

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -5,3 +5,16 @@ resource "aws_s3_bucket" "ec2_bucket" {
     Name = "ec2_bucket"
   }
 }
+
+resource "terraform_data" "upload_input" {
+  provisioner "local-exec" {
+    command = "python script/s3_upload.py"
+    environment = {
+      BUCKET_NAME     = var.bucket_name
+      INPUT_KEY       = var.input_key
+      LOCAL_FILE_PATH = var.local_filepath
+    }
+  }
+
+  depends_on = [aws_s3_bucket.ec2_bucket]
+}
